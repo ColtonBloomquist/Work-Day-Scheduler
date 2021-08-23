25fd51fe1056d8737f7c835fed9245ac52e0ee81
colton bloomquist <coltonbloomquist@yahoo.com>
2021-08-23 13:19:30 -0600

    added save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Display day and date for today
+// Display today's date
 var todaysDate = moment().format("dddd, MMM Do YYYY, h:mm a");
 $("#currentDay").html(todaysDate);
 
@@ -10,7 +10,7 @@ function timeTracker() {
   $(".time-block").each(function () {
     var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-    // Verify time and add/remove classes classes
+    // Verify time and add/remove classes
     if (blockTime < currentTime) {
       $(this).removeClass("future");
       $(this).removeClass("present");
@@ -26,3 +26,36 @@ function timeTracker() {
     }
   });
 }
+
+var saveTask = function () {
+  localStorage.setItem(time, text);
+};
+
+$(document).ready(function () {
+  // saveBtn click listener
+  $(".saveBtn").on("click", function () {
+    // Get nearby values
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    // Save task
+    var saveTask = function () {
+      localStorage.setItem(time, text);
+    };
+    saveTask();
+  });
+
+  // get tasks
+  $("#hour7 .description").val(localStorage.getItem("hour7"));
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour15.5 .description").val(localStorage.getItem("hour15.5"));
+
+  timeTracker();
+});
