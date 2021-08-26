9d45e66d0ab2b3d8c961f3c9479e480054173beb
colton bloomquist <coltonbloomquist@yahoo.com>
2021-08-26 17:15:04 -0600

    final js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,16 +46,16 @@ $(document).ready(function () {
   });
 
   // get tasks
-  $("#hour7 .description").val(localStorage.getItem("hour7"));
-  $("#hour8 .description").val(localStorage.getItem("hour8"));
-  $("#hour9 .description").val(localStorage.getItem("hour9"));
-  $("#hour10 .description").val(localStorage.getItem("hour10"));
-  $("#hour11 .description").val(localStorage.getItem("hour11"));
-  $("#hour12 .description").val(localStorage.getItem("hour12"));
-  $("#hour13 .description").val(localStorage.getItem("hour13"));
-  $("#hour14 .description").val(localStorage.getItem("hour14"));
-  $("#hour15 .description").val(localStorage.getItem("hour15"));
-  $("#hour15.5 .description").val(localStorage.getItem("hour15.5"));
+  $("#hour7 .taskText").val(localStorage.getItem("hour7"));
+  $("#hour8 .taskText").val(localStorage.getItem("hour8"));
+  $("#hour9 .taskText").val(localStorage.getItem("hour9"));
+  $("#hour10 .taskText").val(localStorage.getItem("hour10"));
+  $("#hour11 .taskText").val(localStorage.getItem("hour11"));
+  $("#hour12 .taskText").val(localStorage.getItem("hour12"));
+  $("#hour13 .taskText").val(localStorage.getItem("hour13"));
+  $("#hour14 .taskText").val(localStorage.getItem("hour14"));
+  $("#hour15 .taskText").val(localStorage.getItem("hour15"));
+  $("#hour15.5 .taskText").val(localStorage.getItem("hour15.5"));
 
   timeTracker();
 });
